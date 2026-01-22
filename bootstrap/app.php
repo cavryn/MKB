9aa03de5d4e0bdf2b7476e7002d2d3db503e5a1a
unknown <sheva7526@gmail.com>
2026-01-22 08:14:25 +0700

    m

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -15,6 +15,8 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => AdminMiddleware::class,
         ]);
+
+        $middleware->append(\App\Http\Middleware\ForceHttpsInProduction::class);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
