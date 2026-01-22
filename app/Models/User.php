dbd0fb0ab1515295a2bff860132fa5480ad9095f
unknown <sheva7526@gmail.com>
2026-01-22 08:41:41 +0700

    m

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,43 +2,27 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable
 {
-    /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'phone',
         'password',
+        'role',  // ← TAMBAHKAN INI
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
     protected function casts(): array
     {
         return [
@@ -48,9 +32,7 @@ class User extends Authenticatable
     }
 
     public function carts()
-{
-    return $this->hasMany(\App\Models\Cart::class);
-}
-
-
+    {
+        return $this->hasMany(\App\Models\Cart::class);
+    }
 }
